6ab8d156ead5e280d3b52bd12d64e2aa7fea972c
Sof4ik <114176960+Sof4ik@users.noreply.github.com>
2022-12-19 17:46:04 +0200

    Add files via upload

---
+++ lab_11/src/components/content/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './style.css';
+import Card from './card';
+
+function Content () {
+ return (
+  <div className="content-container">
+   <Card />
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   v
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+   v
+   Nulla laborum ex id non tempor excepteur minim minim nisi dolore anim proident consectetur. Consequat pariatur dolore voluptate nisi dolor cupidatat. Mollit excepteur reprehenderit do exercitation in. Enim deserunt officia sunt magna tempor in.
+  </div>
+ );
+}
+
+export default Content;
---
+++ lab_11/src/components/navigation/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css';
+
+function Navigation () {
+ return (
+  <nav>
+   <div>Milton</div>
+   <hr />
+   <p>
+    Id sunt laborum reprehenderit eiusmod esse ad deserunt.Occaecat nostrud consectetur cupidatat sint laborum.
+   </p>
+   <hr />
+  </nav>
+ );
+}
+
+export default Navigation;
